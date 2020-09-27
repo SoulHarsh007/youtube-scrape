8b8196dd3a0765c85ae6f45d9dac66e46c2421bc
SoulHarsh007 <harshtheking@hotmail.com>
2020-09-27 11:47:49 +0530

    Code Cleanup

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const scraper = require('./scraper')
+const scraper = require('./scraper');
 const app = express();
 
-//Home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + '/index.html');
 });
-
-//API route
 app.get('/api/search', (req, res) => {
-    scraper.youtube(req.query.q, req.query.page)
-        .then(x => res.json(x))
-        .catch(e => res.send(e));
+  scraper
+    .youtube(req.query.q, req.query.page)
+    .then(x => res.json(x))
+    .catch(e => res.send(e));
 });
-
 app.listen(process.env.PORT || 8080, function () {
   console.log('Listening on port 8080');
 });
