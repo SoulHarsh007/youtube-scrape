540efa7822c7a9b51f4d611936890880bb96de47
SoulHarsh007 <harshtheking@hotmail.com>
2020-10-04 12:56:54 +0530

    add ytpl

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const scraper = require('./scraper');
+const ytpl = require('ytpl');
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,6 +9,11 @@ app.get('/', (req, res) => {
 app.get('/api/search', (req, res) => {
   scraper
     .youtube(req.query.q, req.query.page)
+    .then(x => res.json(x))
+    .catch(e => res.send(e));
+});
+app.get('/api/playlist', (req, res) => {
+  ytpl(req.query.q)
     .then(x => res.json(x))
     .catch(e => res.send(e));
 });
